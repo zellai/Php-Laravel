4562ec5b9481592e87b21b865de704cc78250227
Ezel <zellai022890@gmail.com>
2023-05-03 16:05:31 +0800

    Client Info

--- website2/index.php
+++ website2/index.php
@@ -18,6 +18,18 @@
             <?php endforeach; ?>
             </ul>
         <?php endif; ?>
+
+        <h1>Client Info</h1>
+        <?php if($server): ?>
+            <ul class="list-group">
+            <?php foreach($client as $key => $value): ?>
+                <li class="list-group-item">
+                    <strong><?php echo $key ?>: </strong>
+                    <?php echo $value; ?>
+                </li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
      </div>
 </body>
 </html>
