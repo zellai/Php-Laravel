09c9f9b482f1094ad6ae862dd21923048174152b
Ezel <zellai022890@gmail.com>
2023-05-05 15:18:52 +0800

    Unset Cookie

--- website5/page2.php
+++ website5/page2.php
@@ -1,4 +1,9 @@
 <?php
+    setcookie('username', 'Frank', time() + (86400 * 30));
+
+    // Delete cookie
+    setcookie('username', 'Frank', time() - 3600);
+
     if(isset($_COOKIE['username'])){
         echo 'User ' .$_COOKIE['username'] . ' is set <br>' ;
 
