a8b8e1ac1a7759fc883c0aecc2e45ea24b61e5b4
Ezel <zellai022890@gmail.com>
2023-05-08 16:36:03 +0800

    OOP Private

--- oop.php
+++ oop.php
@@ -1,10 +1,20 @@
 <?php
     class Person{
-        public $name;
-        public $email;
+        private $name;
+        private $email;
+    
+        public function setName($name){
+            $this->name = $name;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
     }
 
     $person1 = new Person;
 
-    $person1->name = 'John Doe';
-    echo $person1->name;
+    $person1->setName('Liyah Ozella');
+    echo $person1->getName();
+    // $person1->name = 'John Doe';
+    // echo $person1->name;
