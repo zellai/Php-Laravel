a86476438c98c62a498413882390a39e83ff4b56
Ezel <zellai022890@gmail.com>
2023-05-08 16:47:58 +0800

    constructor

--- oop.php
+++ oop.php
@@ -3,18 +3,31 @@
         private $name;
         private $email;
     
+        public function __construct($name, $email){
+            $this->name = $name;
+            $this->email = $email;
+            echo 'Person created <br>';
+        }
         public function setName($name){
             $this->name = $name;
         }
 
         public function getName(){
-            return $this->name;
+            return $this->name. '<br>';
+        }
+
+        public function setEmail($email){
+            $this->email = $email;
+        }
+
+        public function getEmail(){
+            return $this->email. '<br>';
         }
     }
 
-    $person1 = new Person;
+    $person1 = new Person('John Doe', 'jd@gmail.com');
 
-    $person1->setName('Liyah Ozella');
+    // $person1->setName('Liyah Ozella');
     echo $person1->getName();
     // $person1->name = 'John Doe';
     // echo $person1->name;
