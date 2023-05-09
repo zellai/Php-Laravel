f7fedcffc081cf4411decf2615aecba1fc95d1d0
Ezel <zellai022890@gmail.com>
2023-05-09 21:41:35 +0800

    Edit and Delete

--- website8/index.php
+++ website8/index.php
@@ -2,7 +2,7 @@
     require('config/config.php');
     require('config/db.php');
 
-    // Cretae query
+    // Create query
    $query = 'SELECT * FROM posts ORDER BY created_at DESC';
 
    // Get result
--- website8/post.php
+++ website8/post.php
@@ -2,6 +2,20 @@
     require('config/config.php');
     require('config/db.php');
 
+     // Check for Submit
+     if(isset($_POST['delete'])){
+        // Get form data
+            $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
+    
+            $query = "DELETE FROM posts WHERE id = {$delete_id}";
+    
+            if(mysqli_query($conn, $query)){
+                header('Location:'.ROOT_URL.'');
+            } else {
+                echo 'ERROR: '. mysqli_error($conn);
+            }
+        }
+
    // Get ID
    $id = mysqli_real_escape_string($conn, $_GET['id']);
 
@@ -23,11 +37,18 @@
 ?>
 
 <?php include('inc/header.php'); ?>
-            <div class="container">
+            <div class="container bg-light">
                 <a href="<?php echo ROOT_URL; ?>" class="btn btn-secondary">Back</a>
                 <h1><?php echo $post['title']; ?></h1>
                 <small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
                 <p><?php echo $post['body']; ?></p>
                 <!-- <a class="btn btn-default" href="post.php?id=<?php echo $post['id']; ?>">Read More</a> -->
+                <hr>
+                <a href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['id']; ?>" class="btn btn-secondary">Edit</a>
+                <form data-bs-placement="right" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <input type="hidden" name="delete_id" value="<?php echo $post['id']; ?>">
+                    <input type="submit" name= "delete" value="Delete" class="btn btn-danger">
+                </form>
+                
             </div>
 <?php include('inc/footer.php'); ?>
